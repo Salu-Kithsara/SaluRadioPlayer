eefb2487bc371c19f28cfc20abf12d8a01d76b44
Salu-Kithsara <82644280+Salu-Kithsara@users.noreply.github.com>
2021-10-18 20:22:31 +0530

    Update song.py

--- plugins/bot/song.py
+++ plugins/bot/song.py
@@ -1,6 +1,6 @@
 """
-RadioPlayerV3, Telegram Voice Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
+SaluRadioPlayer, Telegram Voice Chat Bot
+Copyright (c) 2021  Asm Safone <https://github.com/Salu-Kithsara>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
